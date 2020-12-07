6dcf2a6f47af3f078777cba428daf28694c29cfd
dvirsivan1994@gmail.com <dvirsivan1994@gmail.com>
2020-12-07 21:24:22 +0200

    second app

--- app/src/main/java/com/example/transportationManagement/UI/loginActivity.java
+++ app/src/main/java/com/example/transportationManagement/UI/loginActivity.java
@@ -1,14 +1,16 @@
-package com.example.transportationManagement;
+package com.example.transportationManagement.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.transportationManagement.R;
+
+public class loginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
